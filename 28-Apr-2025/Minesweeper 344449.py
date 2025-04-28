362ddd27d22601a880b4b78f46518e1af7273269
Edilawit-Manaye <fortune124a@gmail.com>
2025-04-28 16:13:42 -0700

    Created 'Minesweeper' by A2SV Hub

---
+++ 28-Apr-2025/Minesweeper 344449.py
@@ -0,0 +1,35 @@
+# Problem: Minesweeper - https://leetcode.com/problems/minesweeper/
+
+class Solution:
+    def getAdjacentMines(self, board: List[List[str]], x: int, y: int) -> int:
+        numMines = 0
+        for r in range(x - 1, x + 2):
+            for c in range(y - 1, y + 2):
+                if (r >= 0 and r < len(board) and 
+                    c >= 0 and c < len(board[r]) and 
+                    board[r][c] == 'M'):
+                    numMines += 1
+        return numMines
+
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        if not board:
+            return board
+        
+        x, y = click
+        if board[x][y] == 'B' or (board[x][y].isdigit()):
+            return board
+        if board[x][y] == 'M':
+            board[x][y] = 'X'
+        else:
+            numMines = self.getAdjacentMines(board, x, y)
+            if numMines > 0:
+                board[x][y] = str(numMines)
+            else:
+                board[x][y] = 'B'
+                for r in range(x - 1, x + 2):
+                    for c in range(y - 1, y + 2):
+                        if (r >= 0 and r < len(board) and 
+                            c >= 0 and c < len(board[r]) and 
+                            board[r][c] == 'E'):
+                            self.updateBoard(board, (r, c))
+        return board
