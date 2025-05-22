c8da727d9d8f91aae584583655d2628c63e6026d
Edilawit-Manaye <fortune124a@gmail.com>
2025-05-22 21:33:47 +0300

    Created 'Find Champion II' by A2SV Hub

---
+++ 22-May-2025/Find Champion II 358994.py
@@ -0,0 +1,13 @@
+# Problem: Find Champion II - https://leetcode.com/problems/find-champion-ii/
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        incoming = defaultdict(int)
+        for x , y in edges:
+            incoming[y] += 1
+            
+        ans = []
+        for node in range(n):
+            if incoming[node] == 0:
+                ans.append(node)
+        return ans[0] if len(ans) == 1 else -1
