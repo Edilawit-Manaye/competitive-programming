3228850595ce52866b53db95160ff7bc9dc56cee
Edilawit-Manaye <fortune124a@gmail.com>
2025-05-24 21:08:46 +0300

    Created 'Longest Nice Subarray' by A2SV Hub

---
+++ 24-May-2025/Longest Nice Subarray 359485.py
@@ -0,0 +1,19 @@
+# Problem: Longest Nice Subarray - https://leetcode.com/problems/longest-nice-subarray/
+
+from typing import List
+
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        res = 0
+        cur = 0  
+        left = 0 
+        
+        for right in range(len(nums)):
+            while cur & nums[right]:
+                cur ^= nums[left]
+                left += 1
+            cur |= nums[right]
+            res = max(res, right - left + 1)
+        
+    #     return result
+        return res 
