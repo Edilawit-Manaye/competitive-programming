c24effa590ce37470f06d997870a35fb84009156
Edilawit-Manaye <fortune124a@gmail.com>
2025-07-21 12:12:24 -0700

    Created 'Swap Cases' by A2SV Hub

---
+++ 21-Jul-2025/Swap Cases 378435.py
@@ -0,0 +1,8 @@
+# Problem: Swap Cases - https://www.hackerrank.com/challenges/swap-case?isFullScreen=true
+
+def swap_case(s):
+    return s.swapcase()
+
+input_string = 'HackerRank.com presents "Pythonist 2."'
+output_string = swap_case(input_string)
+print(output_string)
