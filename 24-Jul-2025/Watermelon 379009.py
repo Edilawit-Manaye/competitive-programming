225a93b583f9ba420c7c1c8e994f060551be0904
Edilawit-Manaye <fortune124a@gmail.com>
2025-07-24 07:26:52 -0700

    Created 'Watermelon' by A2SV Hub

---
+++ 24-Jul-2025/Watermelon 379009.py
@@ -0,0 +1,12 @@
+# Problem: Watermelon - https://codeforces.com/problemset/problem/4/A
+
+n=int(input())
+is_first= False
+for i in range (1,n-1):
+    if i%2==0 and (n-i)%2==0:
+        is_first=True
+        break
+if is_first:
+    print("YES")
+else:
+    print("NO")
