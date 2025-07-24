a048122c78fe7fd14eace543e9a5e612d94ed305
Edilawit-Manaye <fortune124a@gmail.com>
2025-07-24 07:28:10 -0700

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 24-Jul-2025/Way Too Long Words 379010.py
@@ -0,0 +1,21 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n=int(input())
+alll=[]
+for _ in range (n):
+    wordd=input()
+    if len(wordd)<=10:
+        alll.append(wordd)
+        
+    else:
+        strr=""
+        strr+=(wordd[0])
+        strr+=(str(len(wordd)-2))
+        strr+=(wordd[len(wordd)-1])
+        alll.append(strr)
+
+        
+for l in alll:
+    print(l)
+            
+    
