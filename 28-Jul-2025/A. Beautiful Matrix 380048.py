9cc222a85cd7c82781874e79e6ba3f5bbbef8773
Edilawit-Manaye <fortune124a@gmail.com>
2025-07-28 11:28:11 -0700

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 28-Jul-2025/A. Beautiful Matrix 380048.py
@@ -0,0 +1,12 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix=[]
+for _ in range(5):
+    matrix.append(list(map(int,input().split())))
+count=0
+for r in range(5):
+    for c in range(5):
+        if matrix[r][c]==1:
+            count+=abs(r-2)+abs(c-2)
+            break
+print(count)
