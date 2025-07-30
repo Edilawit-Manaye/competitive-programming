4ba155191b8425b7a564bde91a2db872a09d5f5f
Edilawit-Manaye <fortune124a@gmail.com>
2025-07-30 14:12:46 -0700

    Created 'Petya and Strings' by A2SV Hub

---
+++ 30-Jul-2025/Petya and Strings 380722.py
@@ -0,0 +1,13 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+str1=input()
+str2=input()
+if str1.lower()==str2.lower():
+    print("0")
+for i in range(len(str1)):
+    if ord(str1[i].lower()) < ord(str2[i].lower()):
+        print("-1")
+        break
+    elif ord(str1[i].lower()) > ord(str2[i].lower()):
+        print("1")
+        break
