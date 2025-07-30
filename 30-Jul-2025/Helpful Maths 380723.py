37499465b8403331cc770eb041d6c170c0a0b55a
Edilawit-Manaye <fortune124a@gmail.com>
2025-07-30 14:12:58 -0700

    Created 'Helpful Maths' by A2SV Hub

---
+++ 30-Jul-2025/Helpful Maths 380723.py
@@ -0,0 +1,19 @@
+# Problem: Helpful Maths - https://codeforces.com/problemset/problem/339/A
+
+t=input()
+strr=[]
+for n in t:
+    if n!="+":
+        strr.append(n)
+strr2=[]
+strr.sort()
+for i in range(len(strr)):
+    if i==len(strr)-1:
+        strr2.append(strr[i])
+    else:
+        strr2.append((strr[i]) + ("+"))
+print("".join(strr2))
+
+
+
+
