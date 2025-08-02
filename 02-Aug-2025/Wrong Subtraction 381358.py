db254105f0c936c14a59c07ccc9a009520bffd46
Edilawit-Manaye <fortune124a@gmail.com>
2025-08-02 08:17:03 -0700

    Created 'Wrong Subtraction' by A2SV Hub

---
+++ 02-Aug-2025/Wrong Subtraction 381358.py
@@ -0,0 +1,16 @@
+# Problem: Wrong Subtraction - https://codeforces.com/problemset/problem/977/A?mobile=false
+
+n,k=map(int,input().split())
+m=str(n)
+i=0
+count=int(m)
+l=m
+while count and l and  i <k:
+    if l[-1]!="0":
+        count=count-1
+        l=str(count)
+    else:
+        count=count//10
+        l=str(count)
+    i+=1
+print(count)
