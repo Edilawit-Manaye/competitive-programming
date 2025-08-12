e59fd460887d5fa00b22da7fc7d6a3b53fb0d6e7
Edilawit-Manaye <fortune124a@gmail.com>
2025-08-12 13:40:21 -0700

    Created 'Young Physicist' by A2SV Hub

---
+++ 12-Aug-2025/Young Physicist 384397.py
@@ -0,0 +1,12 @@
+# Problem: Young Physicist - https://codeforces.com/problemset/problem/69/A
+
+t=int(input())
+l,m,n=0,0,0
+for _ in range(t):
+    x,y,z=map(int,input().split())
+    l,m,n=x+l,y+m,z+n
+if l==0 and m==0 and n==0:
+    print("YES")
+else:
+    print("NO")
+
