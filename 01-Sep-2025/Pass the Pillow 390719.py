772a88bdc57d91eb84afaf73daaa6b5777e74904
Edilawit-Manaye <fortune124a@gmail.com>
2025-09-01 13:21:09 -0700

    Created 'Pass the Pillow' by A2SV Hub

---
+++ 01-Sep-2025/Pass the Pillow 390719.py
@@ -0,0 +1,11 @@
+# Problem: Pass the Pillow - https://leetcode.com/problems/pass-the-pillow/description/
+
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        cycle_length = 2 * (n - 1)
+        effective_time = time % cycle_length
+
+        if effective_time <= n - 1:
+            return effective_time + 1
+        else:
+            return cycle_length - effective_time + 1
