150b6591a1c6bc64490eca48d6f1ab63b732845c
Edilawit-Manaye <fortune124a@gmail.com>
2025-09-10 13:16:34 -0700

    Created 'Fizz Buzz' by A2SV Hub

---
+++ 10-Sep-2025/Fizz Buzz 392512.py
@@ -0,0 +1,16 @@
+# Problem: Fizz Buzz - https://leetcode.com/problems/fizz-buzz/description/
+
+from typing import List
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        an = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                an.append("FizzBuzz")
+            elif i % 3 == 0:
+                an.append("Fizz")
+            elif i % 5 == 0:
+                an.append("Buzz")
+            else:
+                an.append(str(i))
+        return an
