a52500a5ef0f70d859dcea5f7c7dd399913c5c8c
Edilawit-Manaye <fortune124a@gmail.com>
2025-10-23 22:51:13 +0300

    Created 'Excel Sheet Column Title' by A2SV Hub

---
+++ 23-Oct-2025/Excel Sheet Column Title 405400.py
@@ -0,0 +1,13 @@
+# Problem: Excel Sheet Column Title - https://leetcode.com/problems/excel-sheet-column-title/description/?envType=problem-list-v2&envId=string
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = []
+        
+        while columnNumber > 0:
+            columnNumber -= 1  
+            remain = columnNumber % 26
+            res.append(chr(remain + ord('A')))
+            columnNumber //= 26 
+        return ''.join(res[::-1])
+        
