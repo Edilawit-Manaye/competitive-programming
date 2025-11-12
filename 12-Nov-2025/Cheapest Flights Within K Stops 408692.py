a0098e4308a8183cff389fd62abd30f43e799df8
Edilawit-Manaye <fortune124a@gmail.com>
2025-11-12 13:09:05 -0800

    Created 'Cheapest Flights Within K Stops' by A2SV Hub

---
+++ 12-Nov-2025/Cheapest Flights Within K Stops 408692.py
@@ -0,0 +1,13 @@
+# Problem: Cheapest Flights Within K Stops - https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prev=[float("inf")]*n
+        prev[src]=0
+        for i in range(k+1):
+            cur=prev[:]
+            
+            for u,v,w in flights:
+                cur[v]=min(cur[v],prev[u]+w)
+            prev=cur
+        return prev[dst] if prev[dst]!=float("inf") else -1
